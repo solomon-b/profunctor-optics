3266dbd72a0442cd479e61a1ade80fbb93b00cef
Solomon Bothwell <sbothwell@cj.com>
2020-03-17 11:42:54 -0700

    Initial notes on concrete optics

--- default.nix
+++ default.nix
@@ -1,9 +1,9 @@
-{ mkDerivation, base, invertible, profunctors, stdenv }:
+{ mkDerivation, base, invertible, mtl, profunctors, stdenv }:
 mkDerivation {
   pname = "profunctor-optics";
   version = "0.1.0";
   src = ./.;
-  libraryHaskellDepends = [ base invertible profunctors ];
+  libraryHaskellDepends = [ base invertible mtl profunctors ];
   homepage = "http://www.github.com/ssbothwell/profunctor-optics";
   description = "A prototype implementation of Profunctor Optics";
   license = stdenv.lib.licenses.bsd3;
